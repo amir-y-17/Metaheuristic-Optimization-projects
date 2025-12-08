e9d463be6ce8107c784b25fc1cfe9396b5affa9f
Amir <am17iryaghoobi@gmail.com>
2025-12-08 14:07:40 +0330

    Create distance matrix

---
+++ Train1/TSP/instance.py
@@ -0,0 +1,29 @@
+import math
+
+
+def distance_matrix(coords):
+    """
+    Compute the distance matrix for a list of coordinates.
+    """
+    n = len(coords)
+    distances = [[0] * n for _ in range(n)]
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            dx = coords[i][0] - coords[j][0]
+            dy = coords[i][1] - coords[j][1]
+            dist = round(math.sqrt(dx**2 + dy**2))
+            distances[i][j] = dist
+            distances[j][i] = dist
+
+    return distances
+
+
+class TSPInstance:
+    """
+    Class representing a TSP instance with coordinates and distance matrix.
+    """
+
+    def __init__(self, coords):
+        self.coords = coords
+        self.distance_matrix = distance_matrix(coords)
